c493aa28b34771d2f294280ba2cc78ce55bb4636
pdicko <48698725+pdicko@users.noreply.github.com>
2023-08-21 01:41:07 -0700

    Make clear, delete and decimal buttons work
    Clear, delete, and decimal buttons now working. Running into problem
    with the numbers being changed back and forth between strings and
    numbers. Still need to fix chaining - if two numbers and an operator
    have been input user can either hit equals to get the solution
    (which is currently working), or user can hit another operator,
    which automatically solves the current equation, makes the solution
    num1 and waits for num2.

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ let num1;
 let num2;
 let operator;
 let currentNum = '';
+let lastButton;
 
 
 calculator.addEventListener('click', (e) => {
@@ -17,19 +18,25 @@ calculator.addEventListener('click', (e) => {
   if (e.target.classList.contains('button')) {
 
     if (e.target.classList.contains('number')) {
+        if (lastButton === 'equals') {
+          return
+        }
       currentNum += e.target.textContent
       display.textContent = currentNum;
+      lastButton = 'number';
     }
 
     if (e.target.classList.contains('operator')) {
       operator = e.target.id;
       num1 = currentNum;
       currentNum = '';
+      lastButton = 'operator'
     }
 
     if (e.target.classList.contains('equals')) {
       num2 = currentNum;
       operate(num1, num2, operator)
+      lastButton = 'equals'
     }
 
     if (e.target.id === 'clear') {
@@ -38,34 +45,44 @@ calculator.addEventListener('click', (e) => {
       operator = '';
       currentNum = '';
       display.textContent = 0;
+      lastButton = 'clear'
     }
 
     if (e.target.id === 'delete') {
+      if (lastButton === 'equals') {return}
       currentNum = currentNum.slice(0, -1);
       display.textContent = currentNum;
+      lastButton = 'delete'
+    }
+
+    if (e.target.id === 'decimal') {
+      if (currentNum % 1 != 0) {return}
+      currentNum += '.';
+      display.textContent = currentNum;
+      lastButton = decimal
     }
   }
 });
 
-function operate(num1, num2, operator) {
+function operate(number1, number2, operator) {
 
   switch (operator) {
     case 'add':
-      currentNum = sumOf(num1, num2);      
+      currentNum = sumOf(number1, number2);      
       break;
     case 'subtract':
-      currentNum = differenceOf(num1, num2);
+      currentNum = differenceOf(number1, number2);
       break;
     case 'multiply':
-      currentNum = productOf(num1, num2);
+      currentNum = productOf(number1, number2);
       break;
     case 'divide':
-      currentNum = quotientOf(num1, num2);
+      currentNum = quotientOf(number1, number2);
       break;
     default:
       break;
   }
 
-  display.textContent = currentNum;
+  display.textContent = Number(currentNum.toFixed(5));
   num1 = currentNum;
 }
