936eb224e84d9f08ad1304140d38ece50abc6041
pdicko <48698725+pdicko@users.noreply.github.com>
2023-08-20 19:05:10 -0700

    Implment basic calculator function.
    Calculator can now operate on two numbers. Need to implement clear, delete, and decimal functions. Need to implement chaning operations. Also, after equation
    is evaluated, pressing number buttons appends them to the solution number.

--- script.js
+++ script.js
@@ -1,82 +1,58 @@
 const calculator = document.querySelector('.calculator');
 const display = document.querySelector('.display');
 
-calculator.addEventListener('click', (e) => {
-
-  if (e.target.classList.contains('button')) {
-
-    getButton(e.target)
-
-    // if (e.target.classList.contains('number')) {
-    //   populateDisplay(e.target.textContent) 
-
-    // } else if  (e.target.classList.contains('operator')) {
-      
-    // }
-
-    // switch (e.target.id) {
-    //   case 'clear':
-    //     console.log(e.target.id)
-    //     break;
-    //   case 'delete':
-    //     console.log(e.target.id)
-    //     break;
-    //   case 'decimal':
-    //     console.log(e.target.id)
-    //     break;
-    //   case 'equals':
-    //     console.log(e.target.id)
-    //   default:
-    //     break;
-    // }
-
-  }
-});
-
-const sumOf = (num1, num2) => num1 + num2;
-const differenceOf = (num1, num2) => num1 - num2;
-const productOf = (num1, num2) => num1 * num2;
-const quotientOf = (num1, num2) => num1 / num2;
+const sumOf = (num1, num2) => +num1 + +num2;
+const differenceOf = (num1, num2) => +num1 - +num2;
+const productOf = (num1, num2) => +num1 * +num2;
+const quotientOf = (num1, num2) => +num1 / +num2;
 
 let num1;
 let num2;
 let operator;
-let displayValue;
+let currentNum = '';
+
+
+calculator.addEventListener('click', (e) => {
+
+  if (e.target.classList.contains('button')) {
+
+    if (e.target.classList.contains('number')) {
+      currentNum += e.target.textContent
+      display.textContent = currentNum;
+    }
+
+    if (e.target.classList.contains('operator')) {
+      operator = e.target.textContent;
+      num1 = currentNum;
+      currentNum = '';
+    }
+
+    if (e.target.classList.contains('equals')) {
+      num2 = currentNum;
+      operate(num1, num2, operator)
+    }
+  }
+});
 
 function operate(num1, num2, operator) {
 
-  return operator(num1, num2);
-}
+  switch (operator) {
+    case '+':
+      currentNum = sumOf(num1, num2);      
+      break;
+    case '-':
+      currentNum = differenceOf(num1, num2);
+      break;
+    case '*':
+      currentNum = productOf(num1, num2);
+      break;
+    case '/':
+      currentNum = quotientOf(num1, num2);
+      break;
+    default:
+      break;
+  }
 
-function getButton(target) {
-    switch (target.id) {
-      case 'clear':
-        console.log(target.id)
-        break;
-      case 'delete':
-        console.log(target.id)
-        break;
-      case 'decimal':
-        console.log(target.id)
-        break;
-      case 'equals':
-        console.log(target.id)
-      case 'zero':
-      case 'one':
-      case 'two':
-      case 'three':
-      case 'four':
-      case 'five':
-      case 'six':
-      case 'seven':
-      case 'eight':
-      case 'nine':
-        populateDisplay(target.textContent)
-      default:
-        break;
-    }
-}
-
-function populateDisplay(number) {
-  display.textContent += number;
+  display.textContent = currentNum;
+  num1 = currentNum;
 }
