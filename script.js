cff17f713a33bce97aa8b05e40f5fd9f91330417
pdicko <48698725+pdicko@users.noreply.github.com>
2023-08-22 15:01:00 -0700

    Finish handler function for number inputs.
    Clicking a number on the UI updates the display. It also updates the
    relevant number in the current equation object.

--- script.js
+++ script.js
@@ -6,13 +6,6 @@ const differenceOf = (num1, num2) => num1 - num2;
 const productOf = (num1, num2) => num1 * num2;
 const quotientOf = (num1, num2) => num1 / num2;
 
-// let num1 = 0;
-// let num2 = 0;
-// let operator = '';
-// let currentNum;
-// let lastButton;
-// let previousNum2;
-
 class Equation {
     constructor () {
       this.num1 = '';
@@ -28,18 +21,26 @@ class Equation {
 let currentEquation = new Equation();
 let currentDisplay;
 
-function handleInput (input) {
+function updateDisplay(toAdd) {
+  if (display.textContent === '0') {
+    display.textContent = toAdd;
+  } else {
+    display.textContent += toAdd;
+  }
 
-  const inputTarget = input;
-  if (inputTarget.classList.contains('number')) {
-    handleNumberInput(input);
+  return display.textContent;
+}
+
+function handleInput (eventTarget) {
+
+  if (eventTarget.classList.contains('number')) {
+    handleNumberInput(eventTarget);
   }
 }
 
-function handleNumberInput(input) {
-  const inputTarget = input.textContent;
-  let inputValue = Number(inputTarget);
+function handleNumberInput(eventTarget) {
 
+  const toAdd = eventTarget.textContent; //is a String
 
   if (currentEquation.operator === '') {
     number = 'num1';
@@ -47,92 +48,79 @@ function handleNumberInput(input) {
     number = 'num2';
   }
 
-  let asString = currentEquation[number].toString();
-  asString += inputValue;
-  currentEquation[number] = Number(asString);
-  display.textContent = currentEquation[number];
+  const newNumber = Number(updateDisplay(toAdd));
+  currentEquation[number] = newNumber;
 }
 
 calculator.addEventListener('click', (e) => {
 
   if (e.target.classList.contains('button')) {
-
       handleInput(e.target);
-    // if (e.target.classList.contains('number')) {
-    //     if (lastButton === 'equals') {
-    //       return
-    //     }
-
-    //   currentNum += e.target.textContent;
-    //   display.textContent = currentNum;
-    //   lastButton = 'number';
-
-
     }
 
-    if (e.target.classList.contains('operator')) {
+  if (e.target.classList.contains('operator')) {
 
-      if (lastButton === 'operator') {
-        operator = e.target.id
-      };
+    if (lastButton === 'operator') {
+      operator = e.target.id
+    };
 
-      if (operator != '') {
-        num2 = currentNum;
-        operate(num1, num2, operator);
-        lastButton = 'operator'
-      }
-
-      if (lastButton === 'equals') {
-        operator = e.target.id;
-        display.textContent = currentNum;
-      }
-
-      operator = e.target.id;
-      num1 = currentNum;
-      currentNum = '';
-      lastButton = 'operator';
-    }
-
-    if (e.target.classList.contains('equals')) {
-      if (num1 === 0) {return};
-
-      if (lastButton === 'equals') {
-        num2 = previousNum2;
-        operate(num1, num2, operator);
-        lastButton = 'equals';
-        return;
-      }
-
+    if (operator != '') {
       num2 = currentNum;
-      operate(num1, num2, operator)
-      lastButton = 'equals'
+      operate(num1, num2, operator);
+      lastButton = 'operator'
     }
 
-    if (e.target.id === 'clear') {
-      num1 = 0;
-      num2 = 0;
-      operator = '';
-      currentNum = '';
-      display.textContent = 0;
-      lastButton = 'clear'
-    }
-
-    if (e.target.id === 'delete') {
-      if (lastButton === 'equals') {return}
-      let asString = currentNum.toString();
-      asString = asString.slice(0, -1);
-      currentNum = Number(asString);
-      if (currentNum === 0) {currentNum = ''}
+    if (lastButton === 'equals') {
+      operator = e.target.id;
       display.textContent = currentNum;
-      lastButton = 'delete';
     }
 
-    if (e.target.id === 'decimal') {
-      if (currentNum % 1 != 0) {return}
-      currentNum += '.';
-      display.textContent = currentNum;
-      lastButton = decimal;
+    operator = e.target.id;
+    num1 = currentNum;
+    currentNum = '';
+    lastButton = 'operator';
+  }
+
+  if (e.target.classList.contains('equals')) {
+    if (num1 === 0) {return};
+
+    if (lastButton === 'equals') {
+      num2 = previousNum2;
+      operate(num1, num2, operator);
+      lastButton = 'equals';
+      return;
     }
+
+    num2 = currentNum;
+    operate(num1, num2, operator)
+    lastButton = 'equals'
+  }
+
+  if (e.target.id === 'clear') {
+    num1 = 0;
+    num2 = 0;
+    operator = '';
+    currentNum = '';
+    display.textContent = 0;
+    lastButton = 'clear'
+  }
+
+  if (e.target.id === 'delete') {
+    if (lastButton === 'equals') {return}
+    let asString = currentNum.toString();
+    asString = asString.slice(0, -1);
+    currentNum = Number(asString);
+    if (currentNum === 0) {currentNum = ''}
+    display.textContent = currentNum;
+    lastButton = 'delete';
+  }
+
+  if (e.target.id === 'decimal') {
+    if (currentNum % 1 != 0) {return}
+    currentNum += '.';
+    display.textContent = currentNum;
+    lastButton = decimal;
+  }
 });
 
 function operate(number1, number2, operator) {
