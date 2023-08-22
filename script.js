45af9b3f37e12bd915b79dc17b608a62da1fdb2b
pdicko <48698725+pdicko@users.noreply.github.com>
2023-08-21 22:11:15 -0700

    Made a mess.
    Code doesn't make any sense anymore. Gonna branch and try to
    clean it up.

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ const differenceOf = (num1, num2) => +num1 - +num2;
 const productOf = (num1, num2) => +num1 * +num2;
 const quotientOf = (num1, num2) => +num1 / +num2;
 
-let num1;
-let num2;
-let operator;
+let num1 = 0;
+let num2 = 0;
+let operator = '';
 let currentNum = '';
 let lastButton;
+let previousNum2;
 
 
 calculator.addEventListener('click', (e) => {
@@ -21,27 +22,53 @@ calculator.addEventListener('click', (e) => {
         if (lastButton === 'equals') {
           return
         }
-      currentNum += e.target.textContent
+
+      currentNum += e.target.textContent;
       display.textContent = currentNum;
       lastButton = 'number';
     }
 
     if (e.target.classList.contains('operator')) {
+
+      if (lastButton === 'operator') {
+        operator = e.target.id
+      };
+
+      if (operator != '') {
+        num2 = currentNum;
+        operate(num1, num2, operator);
+        lastButton = 'operator'
+      }
+
+      if (lastButton === 'equals') {
+        operator = e.target.id;
+        display.textContent = currentNum;
+      }
+
       operator = e.target.id;
       num1 = currentNum;
       currentNum = '';
-      lastButton = 'operator'
+      lastButton = 'operator';
     }
 
     if (e.target.classList.contains('equals')) {
+      if (num1 === 0) {return};
+
+      if (lastButton === 'equals') {
+        num2 = previousNum2;
+        operate(num1, num2, operator);
+        lastButton = 'equals';
+        return;
+      }
+
       num2 = currentNum;
       operate(num1, num2, operator)
       lastButton = 'equals'
     }
 
     if (e.target.id === 'clear') {
-      num1 = '';
-      num2 = '';
+      num1 = 0;
+      num2 = 0;
       operator = '';
       currentNum = '';
       display.textContent = 0;
@@ -50,34 +77,45 @@ calculator.addEventListener('click', (e) => {
 
     if (e.target.id === 'delete') {
       if (lastButton === 'equals') {return}
-      currentNum = currentNum.slice(0, -1);
+      let asString = currentNum.toString();
+      asString = asString.slice(0, -1);
+      currentNum = Number(asString);
+      if (currentNum === 0) {currentNum = ''}
       display.textContent = currentNum;
-      lastButton = 'delete'
+      lastButton = 'delete';
     }
 
     if (e.target.id === 'decimal') {
       if (currentNum % 1 != 0) {return}
       currentNum += '.';
       display.textContent = currentNum;
-      lastButton = decimal
+      lastButton = decimal;
     }
   }
 });
 
 function operate(number1, number2, operator) {
 
+  let first = Number(number1);
+  let second = Number(number2);
+
+  if (operator === 'divide' && second === 0) {
+    display.textContent = 'Err';
+    return
+  }
+
   switch (operator) {
     case 'add':
-      currentNum = sumOf(number1, number2);      
+      currentNum = sumOf(first, second);      
       break;
     case 'subtract':
-      currentNum = differenceOf(number1, number2);
+      currentNum = differenceOf(first, second);
       break;
     case 'multiply':
-      currentNum = productOf(number1, number2);
+      currentNum = productOf(first, second);
       break;
     case 'divide':
-      currentNum = quotientOf(number1, number2);
+      currentNum = quotientOf(first, second);
       break;
     default:
       break;
@@ -85,4 +123,5 @@ function operate(number1, number2, operator) {
 
   display.textContent = Number(currentNum.toFixed(5));
   num1 = currentNum;
+  previousNum2 = second;
 }
